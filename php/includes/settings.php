f2932d00d251ccb6014295f8295cb94f6f5511e9
t0masu <t0masu@users.noreply.github.com>
2014-12-07 21:44:06 +0000

    Update settings.php

--- php/includes/settings.php
+++ php/includes/settings.php
@@ -1,4 +1,4 @@
 <?php
 	include(__DIR__ . '/../../php/class/main.php');
-	$ControlPanel = new ControlPanel("127.0.0.1", "c0cpanelDEV", "c0cpaneldev", "rqSkRIEp!7"); //client customisable
+	$ControlPanel = new ControlPanel("127.0.0.1", "REPLACE ME", "REPLACE ME", "PASSWORD"); //client customisable
 ?>
