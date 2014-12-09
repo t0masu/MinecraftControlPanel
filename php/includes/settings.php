6ebd8f9ff2cc0267c04c48293310c6b8ed815765
t0masu <t0masu@users.noreply.github.com>
2014-12-09 14:07:44 +0000

    Change to correct vars

--- php/includes/settings.php
+++ php/includes/settings.php
@@ -1,4 +1,4 @@
 <?php
 	include(__DIR__ . '/../../php/class/main.php');
-	$ControlPanel = new ControlPanel("127.0.0.1", "REPLACE ME", "REPLACE ME", "PASSWORD"); //client customisable
+	$ControlPanel = new ControlPanel("127.0.0.1", "DATABASE NAME", "DATABASE USER", "DATABASE PASSWORD"); //client customisable
 ?>
